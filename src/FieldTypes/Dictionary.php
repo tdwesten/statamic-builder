f7e0bd73e49c9ed636c3111c367cc48b0042ed31
Matthias van Pijkeren <matthias@pixelpillow.nl>
2024-10-14 13:59:53 +0200

    Added placeholder and maxItems to dictionary field

--- src/FieldTypes/Dictionary.php
+++ src/FieldTypes/Dictionary.php
@@ -3,17 +3,21 @@
 namespace Tdwesten\StatamicBuilder\FieldTypes;
 
 use Illuminate\Support\Collection;
+use Tdwesten\StatamicBuilder\Contracts\MaxItems;
 use Tdwesten\StatamicBuilder\Contracts\Makeble;
 
 class Dictionary extends Field
 {
     use Makeble;
+    use MaxItems;
 
     protected $type = 'dictionary';
 
     protected string $dictionaryType = '';
 
     protected array $dictionaryOptions = [];
+
+    protected array $placeholder;
 
     public function __construct(string $handle)
     {
@@ -27,6 +31,8 @@ class Dictionary extends Field
                 'type' => $this->dictionaryType,
                 ...$this->dictionaryOptions,
             ],
+            'placeholder' => $this->placeholder,
+            'max_items' => $this->max_items,
         ]);
     }
 
@@ -46,6 +52,15 @@ class Dictionary extends Field
     public function dictionaryOptions(array $attributes): self
     {
         $this->dictionaryOptions = $attributes;
+
+        return $this;
+    }
+
+    /**
+     * Set the non-selectable placeholder text. Default: none.
+     */
+    public function placeholder(string $placeholder): self {
+        $this->placeholder = $placeholder;
 
         return $this;
     }
