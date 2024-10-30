dbe6dfa22858cd4088e283bffa02e93c53da9c20
Thomas van der Westen <thomas@pixelpillow.nl>
2024-10-30 15:39:51 +0100

    Added collections tests

--- tests/Unit/CollectionTest.php
+++ tests/Unit/CollectionTest.php
@@ -1,5 +1,6 @@
 <?php
 
+use Tests\Helpers\MultisiteTestCollection;
 use Tests\Helpers\TestCollection;
 
 test('Has a title', function () {
@@ -8,4 +9,45 @@ test('Has a title', function () {
     expect($collection->title())->toBe(
         'Shows'
     );
+});
+
+test('Has a handle', function () {
+    $collection = new TestCollection();
+
+    expect($collection->handle())->toBe(
+        'shows'
+    );
+});
+
+test('Has a route', function () {
+    $collection = new TestCollection();
+
+    expect($collection->route())->toBe(
+        '/shows/{slug}'
+    );
+});
+
+test('Has multiple route for multisites', function () {
+    $collection = new MultisiteTestCollection();
+
+    expect($collection->route())->toBe(
+        [
+            'en' => '/shows/{slug}',
+            'de' => '/shows/{slug}',
+        ]
+    );
+});
+
+test('Can have multiple sites', function () {
+    $collection = new MultisiteTestCollection();
+
+    expect($collection->sites())->toBe(
+        ['en', 'de']
+    );
+});
+
+test('Has slugs', function () {
+    $collection = new TestCollection();
+
+    expect($collection->slugs())->toBeTrue();
 });
