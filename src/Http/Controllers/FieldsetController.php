fcebfd41e5982c9e936c44b409a3b30420c3ce63
Tyler Dakin <tyler@dakin.one>
2025-03-17 22:15:45 -0400

    Add editor links to fieldset

--- src/Http/Controllers/FieldsetController.php
+++ src/Http/Controllers/FieldsetController.php
@@ -2,6 +2,7 @@
 
 namespace Tdwesten\StatamicBuilder\Http\Controllers;
 
+use ReflectionClass;
 use Statamic\Facades\Fieldset as FieldsetFacade;
 use Statamic\Http\Controllers\CP\Fields\FieldsetController as StatamicFieldsetController;
 
@@ -12,10 +13,12 @@ class FieldsetController extends StatamicFieldsetController
         $builderFieldset = FieldsetFacade::findFieldset($fieldset);
 
         if ($builderFieldset) {
+            $reflection = new ReflectionClass($builderFieldset);
+
             return view('statamic-builder::not-editable', [
                 'type' => 'Fieldset',
                 'isLocal' => config('app.env') === 'local' || config('app.env') === 'development',
-                'filePath' => null,
+                'filePath' => $reflection->getFileName(),
             ]);
         }
 
