5d54437a871ab2061c62a220e48e561ec030a129
kgirzadas <11364908+kgirzadas@users.noreply.github.com>
2026-03-09 17:54:02 +0200

    feat: add collapsible support to sections

--- src/FieldTypes/Section.php
+++ src/FieldTypes/Section.php
@@ -8,7 +8,11 @@ class Section extends Field
 
     protected $displayName;
 
-    public function __construct($displayName, $fields = [])
+    protected ?bool $collapsible = null;
+
+    protected ?bool $collapsed = null;
+
+    public function __construct(string $displayName, array $fields = [])
     {
         $this->displayName = $displayName;
         $this->fields = collect($fields);
@@ -19,12 +23,31 @@ class Section extends Field
         return new static($displayName, $fields);
     }
 
-    public function toArray()
+    public function collapsible(bool $collapsible = true): static
     {
-        return [
+        $this->collapsible = $collapsible;
+
+        return $this;
+    }
+
+    public function collapsed(bool $collapsed = true): static
+    {
+        $this->collapsed = $collapsed;
+
+        return $this;
+    }
+
+    public function toArray(): array
+    {
+        return collect([
             'display' => $this->displayName,
+            'instructions' => $this->instructions,
+            'collapsible' => $this->collapsible,
+            'collapsed' => $this->collapsible === true ? ($this->collapsed ?? false) : null,
             'fields' => $this->fieldsToArray(),
-        ];
+        ])->filter(function ($item) {
+            return ! in_array($item, [null, ''], true);
+        })->toArray();
     }
 
     public function fieldsToArray(): array
